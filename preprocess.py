2a7847d4825ec35a0e365edb0ceb38de6eecdbf3
Hannes Holste <hannes.holste@gmail.com>
2015-11-22 00:15:43 -0800

    preprocess ingredient strings

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,13 @@
 import json
 from pprint import pprint
 from collections import defaultdict
-from ingredients import normalize_ingredient_name
+
+import regex
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 
 class Preprocess:
@@ -11,45 +17,85 @@ class Preprocess:
     ingredient_set = set()
     data = []
     counts = defaultdict(int)
+    food_adjectives = []
 
     def __init__(self):
-        self.load_data()
+        self.load_food_adjectives()
+        self.load_data(func_process_ingredient=self.process_ingredient)
+
+
+    def load_food_adjectives(self):
+        for l in open('data/food_adjectives.txt', 'r'):
+            if len(l) > 0:
+                self.food_adjectives.append(l.replace("\n", ""))
 
     def parseData(self, fname):
         with open(fname) as data_file:
             data = json.load(data_file)
             return data
 
-    @staticmethod
-    def process_ingredient(ingredient_name):
+    def process_ingredient(self, ingredient_name):
+        # strip unicode e.g. \u2012
         ingredient_name = ingredient_name.decode('unicode_escape').encode('ascii','ignore')
 
-        return ingredient_name
+        # strip punctuation
+        ingredient_name = regex.sub(ur"\p{P}+", "", ingredient_name)
+
+        # strip parantheses and their contents
+        # e.g. ( 3  oz.) tomato paste
+        ingredient_name = regex.sub(ur"\(.+\)", "", ingredient_name)
+
+
+        # remove food adjectives
+        for adjective in self.food_adjectives:
+            # only substitute adjectives if they appear as independent words, e.g. replace "or" in "toast or bacon", not "pORk"
+            ingredient_name = regex.sub(u"(" + adjective + " |" + adjective + "or)", " ", ingredient_name)
+
+        # remove excess spaces
+        ingredient_name = " ".join(ingredient_name.split())
+
+        # remove preceding and trailing spaces
+        return ingredient_name.strip()
 
     def load_data(self, func_process_ingredient=lambda s: s):
         default_data_dir = "data/train.json"
         self.data = self.parseData(default_data_dir)
+        clean_data = list()
 
         # For each recipe...
         for datum in self.data:
+            clean_datum = dict()
 
             # process cuisine
             cuisine = datum['cuisine'].lower()
+            clean_datum["cuisine"] = cuisine
 
             self.cuisines[cuisine].append(datum)
             self.cuisines_set.add(cuisine)
 
             # process ingredients
             ingredients = datum['ingredients']
+            clean_ingredients = list()
 
             for ingredient in ingredients:
                 # always lowercase ingredient names
                 ingredient = ingredient.lower()
 
-                #ingredient = func_process_ingredient(ingredient)
+                # clean up ingredient string
+                ingredient = func_process_ingredient(ingredient)
+
+                clean_ingredients.append(ingredient)
+
                 self.counts[(cuisine,ingredient)] += 1
                 self.ingredient_list[ingredient].append(datum)
                 self.ingredient_set.add(ingredient)
+
+            clean_datum["ingredients"] = clean_ingredients
+
+            clean_data.append(clean_datum)
+
+        self.data = clean_data
+
 
     def print_info(self):
         print "Number of recipes:", len(self.data)
@@ -73,4 +119,3 @@ class Preprocess:
         ingredient_count.reverse()
         for i in range(0, 25):
             print ingredient_count[i][0], ingredient_count[i][1]
-

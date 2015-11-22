3a2c9dff088a9f1619f7828ba7fda71dd37132d4
Edward Wong <edward4wong@gmail.com>
2015-11-21 23:16:14 -0800

    Scoring

--- model.py
+++ model.py
@@ -3,6 +3,31 @@ import random
 
 memo = {}
 
+
+def calculate_score1(cuisines_list, cuisine_set, cuisine, ingredient_list, ingredient, counts):
+    score = 0.0
+    recipes_in_cuisine = 0.0
+    recipes_not_in_cuisine = 0.0
+
+    for curr_cuisine in cuisine_set:
+        if curr_cuisine == cuisine:
+            recipes_in_cuisine += len(cuisines_list[curr_cuisine])
+        else:
+            recipes_not_in_cuisine += len(cuisines_list[curr_cuisine])
+
+   
+    if (cuisine, ingredient) in memo.keys():
+        score += memo[(cuisine, ingredient)]
+        return score
+
+    times_ingredient_in_cuisine = counts[(cuisine, ingredient)]
+    times_ingredient_not_in_cuisine = len(ingredient_list[ingredient]) - times_ingredient_in_cuisine
+
+    memo[(cuisine, ingredient)] = times_ingredient_in_cuisine/recipes_in_cuisine - times_ingredient_not_in_cuisine/recipes_not_in_cuisine
+    score += times_ingredient_in_cuisine/recipes_in_cuisine - times_ingredient_not_in_cuisine/recipes_not_in_cuisine
+    return score
+
+
 def calculate_score(cuisines_list, cuisine_set, cuisine, ingredient_list, datum, counts):
     score = 0.0
     recipes_in_cuisine = 0.0
@@ -57,6 +82,9 @@ def main():
     print "Error Rate:", calc_error(predictions, correct_answers)
     baseline = ['italian'] * len(validation_set)
     print "Baseline Error Rate:", calc_error(baseline, correct_answers)
+
+
+
 
 main()
 
